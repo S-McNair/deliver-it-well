ce8e933654cc0f5a929e1dff5b938bd519e6ba14
Shane <shane.mcnair@gmail.com>
2018-05-04 11:32:08 -0700

    added .circleci folder

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('h2 says Circle CI Test', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Circle CI Test');
+        done();
+      });
+    });
 })
